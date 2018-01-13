ffc4fd4782af8c1f493b523718c88b1f3cbb1c02
JJones <jjones@brysontech.com>
2018-01-12 20:22:08 -0500

    Working on the navbar and styling issues

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -19,5 +19,71 @@ class DefaultController extends Controller
         ]);
     }
     
+     /**
+     * @Route("/about_us", name="about_us")
+     */
+    public function indexAboutUs(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('About_Us/Index/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+    
+    /**
+     * @Route("/programs", name="programs")
+     */
+    public function indexPrograms(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('Programs/Index/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+    
+    /**
+     * @Route("/class_schedules", name="class")
+     */
+    public function indexClassSchedules(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('Class_Schedules/Index/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+    
+    /**
+     * @Route("/the_store", name="store")
+     */
+    public function indexTheStore(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('Store/Index/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+    
+    /**
+     * @Route("/news", name="news")
+     */
+    public function indexNews(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('News/Index/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+    
+    /**
+     * @Route("/contact_us", name="contact")
+     */
+    public function indexContactUs(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('Contact_Us/Index/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+    
     
 }
